9b000d30c458ce9e0fefbd99ede8b15265c31254
Tobias Rosenberger <TRosenberger@gmx.de>
2015-07-03 10:32:05 +0200

    Removed double naming of host in run.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 omniidl -bcxx Calc.idl 
-idlj -fall Calc.idl 
+idlj Calc.idl 
 javac CalcImpl.java
 g++ -L/opt/omniORB/lib -lomniORB4 -lomnithread CalcSK.cc CalcClient.cpp -o calc_client
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 ORBInitialPort=1050
 ORBInitialHost=localhost
 
-orbd -ORBInitialPort $ORBInitialPort -ORBInitialHost $ORBInitialHost localhost &
-java CalcImpl -ORBInitialPort $ORBInitialPort -ORBInitialHost ORBInitialHost localhost&
+orbd -ORBInitialPort $ORBInitialPort -ORBInitialHost $ORBInitialHost &
+java CalcImpl -ORBInitialPort $ORBInitialPort -ORBInitialHost ORBInitialHost &
 
 echo "run client with: ./calc_client ${IOR}"

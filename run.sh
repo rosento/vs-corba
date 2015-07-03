6bb200360eeba997823df83305cc5f6c494b7fb8
Tobias Rosenberger <TRosenberger@gmx.de>
2015-07-03 11:01:25 +0200

    Added $ in run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ ORBInitialPort=1050
 ORBInitialHost=localhost
 
 orbd -ORBInitialPort $ORBInitialPort -ORBInitialHost $ORBInitialHost &
-java CalcImpl -ORBInitialPort $ORBInitialPort -ORBInitialHost ORBInitialHost &
+java CalcImpl -ORBInitialPort $ORBInitialPort -ORBInitialHost $ORBInitialHost &
 
 echo "run client with: ./calc_client ${IOR}"
